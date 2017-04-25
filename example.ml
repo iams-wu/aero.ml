7603bf4e194256e84733027b3e934bde3d843fb4
ojmb <ojmb@bonsfire.net>
2017-04-24 23:01:32 -0500

    Rolled back example.ml, rewriting not yet implemented

--- example.ml
+++ example.ml
@@ -1,7 +1,7 @@
 let [] n c = n in
 let ~::~ h t n c = c h t in
 
-rewrite ( ~ :: ~ ) :: ~ -> ~ :: ( ~ :: ~ ) in
+~~ rewrite ( ~ :: ~ ) :: ~ -> ~ :: ( ~ :: ~ ) in
 
 let false f t = f in
 let true f t = t in
@@ -22,10 +22,10 @@ let not bool = bool true false in
 let rec map f list =
   let [] = [] in
   let cons h t =
-    ( f h ) :: ( map f t )
+    ~::~ ( f h ) ( map f t )
   in
   list [] cons
 in
 
-str_list str_bool ( map not ( false :: true :: false :: true :: false :: true :: [] ) )
+str_list str_bool ( map not ( ~::~ false ( ~::~ true ( ~::~ false ( ~::~ true ( ~::~ false ( ~::~ true [] ) ) ) ) ) ) )
 
